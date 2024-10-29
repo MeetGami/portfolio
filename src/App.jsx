940b463eeff6dac4463ab4415de441b6ed603010
MeetGami <meet97gami@gmail.com>
2024-10-29 18:16:07 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,7 @@ import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
 import Home from "./components/home/home";
 const App = () =>{
   return(
-    <Router>
-      <Routes>
-        <Route path="/home" element={<Home/>}/>
-      </Routes>
-    </Router>
+    <Home/>
   )
 }
 export default App;
